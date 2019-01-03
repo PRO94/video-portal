8fa518bed207fdca08b37dce338f3cef120e6a7f
Roman Prytulenko <Roman.Prytulenko@gmail.com>
2019-01-03 12:13:18 +0200

    Add Movie and Genre with logic.

--- VideoPortal/VideoPortal/Controllers/CustomersController.cs
+++ VideoPortal/VideoPortal/Controllers/CustomersController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
 using VideoPortal.Models;
 using System.Data.Entity;
--- VideoPortal/VideoPortal/Controllers/MoviesController.cs
+++ VideoPortal/VideoPortal/Controllers/MoviesController.cs
@@ -1,38 +1,42 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
 using VideoPortal.Models;
+using System.Data.Entity;
 
 namespace VideoPortal.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        public ApplicationDbContext _context { get; set; }
+
+        public MoviesController()
+        {
+            _context = ApplicationDbContext.Create();
+        }
+        
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies
+                .Include(m => m.Genre)
+                .ToList();
 
             return View(movies);
         }
-
-        // GET: Movies/Details/{id}
+        
         public ActionResult Details(int id)
         {
-            var movies = GetMovies();
-            var movie = movies.SingleOrDefault(m => m.Id == id);
+            var movie = _context.Movies
+                .Include(m => m.Genre)
+                .SingleOrDefault(m => m.Id == id);
             if (movie == null)
                 return HttpNotFound();
 
             return View(movie);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        protected override void Dispose(bool disposing)
         {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Movie 1" },
-                new Movie { Id = 2, Name = "Movie 2" }
-            };
+            _context.Dispose();
         }
     }
 }
