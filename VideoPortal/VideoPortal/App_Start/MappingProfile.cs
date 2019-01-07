6e2c4a52faf6529fc757ef2eb7cb3d5954be60e5
Roman Prytulenko <Roman.Prytulenko@gmail.com>
2019-01-07 15:02:04 +0200

    Add Movie and Customer API Controllers with CRUD operations.

---
+++ VideoPortal/VideoPortal/App_Start/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using VideoPortal.Models;
+using VideoPortal.Models.DTOs;
+
+namespace VideoPortal
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<CustomerDto, Customer>();
+
+            CreateMap<Movie, MovieDto>();
+            CreateMap<MovieDto, Movie>();
+        }
+    }
+}
---
+++ VideoPortal/VideoPortal/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace VideoPortal
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Enable receiving cameStyle data format as result on UI
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional}
+            );
+        }
+    }
+}
